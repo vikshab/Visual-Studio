7a2e4ad05dcb3a58921e382c273091a033ef01f1
vikshab - Victoria Shabuni <vikshab2010@hotmail.com>
2015-11-06 22:23:23 -0700

    Created Customer, Movie and Rental models, created customer, movie and rental tables

---
+++ MovieRentalsAPI/MovieRentalsAPI/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MovieRentalsAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RegisteredAt { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public float AccountCredit { get; set; }
+    }
+}
---
+++ MovieRentalsAPI/MovieRentalsAPI/Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalsAPI.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public string CheckoutDate { get; set; }
+        public string ReturnedDate { get; set; }
+        public string RentalTime { get; set; }
+        public string Cost { get; set; }
+        public string Total { get; set; }
+        public int CustomerId { get; set; }
+        public int MovieId { get; set; }
+    }
+}
